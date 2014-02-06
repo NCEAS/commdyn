32b4628f56158f670184c4ac7df68539f85d22af
Julie Ripplinger <julieripplinger@dhcp231.nceas.ucsb.edu>
2014-02-06 15:36:33 -0800

    move arthropods code to new folder

--- workflows-and-analysis/konza/cap_arthropods.R
+++ workflows-and-analysis/konza/cap_arthropods.R
@@ -12,9 +12,6 @@
 # Datasets
 # --------
 # 
-
-
-
 # - ARTHROPODS - CAP
 # - KONZA Grassland
 # - PORTAL small mammal
@@ -24,92 +21,48 @@
 # - Stream inverts
 # 
 # 
-# Konza Grassland data:
-# 
-# - Accessing raw data:
-# 
-# Go to http://www.konza.ksu.edu/knz/pages/data
-# Search for PVC02
-# Download data (maximum date ranges)
-
+# CAP arthropod data:
 arth <- read.csv("~/Dropbox/conferences/NCEAS_workshop_feb2014/lter_arthropods.csv")
 
+column.codes <- colnames(arth)
 
-konza <- read.csv("~/Downloads/PVC021.dat", na.strings=0)
-
-
-column.codes <- colnames(konza)
-
-column.definitions <- c("Data code (factor), same for all data in this dataset", 
-                        RECTYPE = "",
-                        RECYEAR = "Year data recorded (YYYY)", 
-                        RECYEAR1 = "Two digit version of year recorded (YY)",
-                        RECMONTH = "Month data recorded (M)",
-                        RECDAY = "Day data recorded (D)",
-                        WATERSHED = "code describing the location data was measured",
-                        SOILTYPE = "code describing the soil class",
-                        SPECODE = "Species code",
-                        AB_GENUS = "Genus name",
-                        AB_SPECIES = "Species name",
-                        AB_VARIETY = "Species variety",
-                        A1 = "plot 1, transect A",
-                        A2 = "plot 2, transect A",
-#                        ...
-                        E1,
-                        DateInserted = "Date data was uploaded"
-                        )
+#building metadata into table. save for later. available at: http://caplter.asu.edu/data/data-catalog/?id=41
+#column.definitions <- c("Data code (factor), same for all data in this dataset", 
+#                        sampling_event_id = "ID number connecting to a sampling event",
+#                         site_id = "Unique numbers representing specific sites", 
+#                         sample_date = "Date the sample was taken",
+#                         default_person_for_trap_samples = "Month data recorded (M)",
+#                         RECDAY = "Day data recorded (D)",
+#                         WATERSHED = "code describing the location data was measured",
+#                         SOILTYPE = "code describing the soil class",
+#                         SPECODE = "Species code",
+#                         AB_GENUS = "Genus name",
+#                         AB_SPECIES = "Species name",
+#                         AB_VARIETY = "Species variety",
+#                         A1 = "plot 1, transect A",
+#                         A2 = "plot 2, transect A",
+# #                        ...
+#                         E1,
+#                         DateInserted = "Date data was uploaded"
+#                         )
 
 
 
-# Data cleanup 
-# 
-# - All `NA` characters should be numeric 0.  
-# 
-# - Extract by transect (watershed), by plot, or by site
-# - Species as columns 
-# - Date as a column
+# Data cleanup
+# replace NULL values in data (columns 14:18) with 0
+size <- sapply(arth[,14:18], function(x) as.numeric(as.character(x)))
+arth[,14:18] <- size
 
-watershed_001d = subset(konza, WATERSHED=="001d")
-watershed_001d_ft <- subset(watershed_001d, SOILTYPE %in% c("f", "t"))
-data.cols <- grep("[A-D][1-9]", colnames(konza))
-species.dates <- which(colnames(dat) %in% c("RECYEAR", "RECMONTH", "RECDAY", "SOILTYPE", "AB_GENUS", "AB_SPECIES", "AB_VARIETY"))
-watershed_001d_ft <- watershed_001d_ft[c(data.cols, species.dates)]
-dat <- watershed_001d_ft 
+# sum size class counts into counts by display_name
+arth$abundance <- rowSums( arth[,14:18], na.rm=TRUE )
 
+# keep only columns of interest
+myvars <- c("site_id", "sample_date", "abundance", "display_name")
+arth2 <- arth[myvars]
 
-# - Combine Species names to single column. (Variety not really necessary)
-
-
-scientific.name = paste(dat$AB_GENUS, dat$AB_SPECIES, dat$AB_VARIETY, sep=" ")
-keepme <- !(colnames(dat) %in% c("AB_GENUS", "AB_SPECIES", "AB_VARIETY"))
-dat <- dat[keepme]
-dat$scientific.name <- scientific.name
-
-
-# Combine dates into a single column
-
-
-
-# translate abundance codes: code corresponds the midpoint of the percent cover range.  
-
-abundance_codes <- 
-c("0" = 0,
-  "1" = 0.5,
-  "2" = 3,
-  "3" = 17.5,
-  "4" = 37.5,
-  "5" = 62.5,
-  "6" = 85,
-  "7" = 98.5)
-
-data.cols <- grep("[A-D][1-9]", colnames(dat))
-
-sapply(dat, function(x){
-  x <- as.character(x)
-  x[x==""] <- "0"
-  abundance_codes[x]
-}
-
-
+# Reshape data from tabular into site X species matrix with abundance as value
+library(reshape2)
+sxs <- dcast(arth2, site_id + sample_date ~ display_name, value.var="abundance")
+head(sxs)
 
 
