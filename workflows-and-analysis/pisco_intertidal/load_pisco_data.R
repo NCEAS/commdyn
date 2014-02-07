8821ca5a4c2a57d45d9bc93592fb3cc62b8c6173
Matt Jones <gitcode@magisa.org>
2014-02-06 17:05:39 -0900

    Eliminted redundant getData() call.

--- workflows-and-analysis/pisco_intertidal/load_pisco_data.R
+++ workflows-and-analysis/pisco_intertidal/load_pisco_data.R
@@ -4,6 +4,7 @@ library(dataone)
 cli <- D1Client()
 
 # search for data packages, listing both data and metadata relevant, and only request versions that are not obsolete
+# for this PISCO search, it allows us to discover the identifier for the package resource map, used below
 results <- d1SolrQuery(cli, list(q="id:*pisco_intertidal.50.6 -obsoletedBy:*",fl="identifier,title,author,documents,resourceMap"))
 sr <- xmlParse(results)
 sr
@@ -15,25 +16,15 @@ getDataFrame <- function(pkg, pid) {
     return(d1)
 }
 
-# Retrieve a whole data package and its contents, and list its identifiers
+# Retrieve the PISCO point count data package, and list its identifiers
 pkg <- getPackage(cli, "resourceMap_pisco_intertidal.50.6")
 getIdentifiers(pkg)
 
 # Create data frames for each of the data objects
 d1 <- getDataFrame(pkg, "doi:10.6085/AA/pisco_intertidal.32.8")
-head(d1)
-
 d2 <- getDataFrame(pkg, "doi:10.6085/AA/pisco_intertidal.45.2")
-head(d2)
-
 d3 <- getDataFrame(pkg, "doi:10.6085/AA/pisco_intertidal.31.8")
-head(d3)
-
 d4 <- getDataFrame(pkg, "doi:10.6085/AA/pisco_intertidal.33.3")
-head(d4)
-
-d4 <- getDataFrame(pkg, "doi:10.6085/AA/pisco_intertidal.33.3")
-head(d4)
 
 # Download the metadata as well
 obj5 <- getMember(pkg, "doi:10.6085/AA/pisco_intertidal.50.6")
