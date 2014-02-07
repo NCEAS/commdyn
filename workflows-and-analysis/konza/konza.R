64713ee809b8fb51e89dd5076aa3ec41b34a7113
Carl Boettiger <cboettig@gmail.com>
2014-02-06 16:32:52 -0800

    failure to group by date interval

--- workflows-and-analysis/konza/konza.R
+++ workflows-and-analysis/konza/konza.R
@@ -1,70 +1,25 @@
-# Group 1.5
-# =========
-# 
-# Group 1: Data set transformation to allow compute of many metrics
-#     Who: Erica Carl Scott Eric Sydney  Julie
-# 
-# Group 2: Time series analysis of community level metrics (consider higher freq data too)
-#     Who: Elizabeth Lauren Carl2 Seekell Julie Andrew
-# 
-# Datasets
-# --------
-# 
-# - ARTHROPODS - CAP
-# - KONZA Grassland
-# - PORTAL small mammal
-# - FISH - Corinna
-# - HUDSON River fisher
-# - Diatoms / Fish / Inverts 
-# - Stream inverts
-# 
-# 
+
 # Konza Grassland data:
-# 
+
+library(reshape2)
+
 # - Accessing raw data:
 # 
 # Go to http://www.konza.ksu.edu/knz/pages/data
 # Search for PVC02
 # Download data (maximum date ranges)
 
-konza <- read.csv("~/Downloads/PVC021.dat", na.strings=0)
-column.codes <- colnames(konza)
+konza <- read.csv("~/Downloads/PVC021.dat", na.strings=0, stringsAsFactors = FALSE)
 
-column.definitions <- c("Data code (factor), same for all data in this dataset", 
-                        RECTYPE = "",
-                        RECYEAR = "Year data recorded (YYYY)", 
-                        RECYEAR1 = "Two digit version of year recorded (YY)",
-                        RECMONTH = "Month data recorded (M)",
-                        RECDAY = "Day data recorded (D)",
-                        WATERSHED = "code describing the location data was measured",
-                        SOILTYPE = "code describing the soil class",
-                        SPECODE = "Species code",
-                        AB_GENUS = "Genus name",
-                        AB_SPECIES = "Species name",
-                        AB_VARIETY = "Species variety",
-                        A1 = "plot 1, transect A",
-                        A2 = "plot 2, transect A",
-#                        ...
-                        E1,
-                        DateInserted = "Date data was uploaded"
-                        )
+# Subset 
+konza <- subset(konza, WATERSHED=="001d")
+konza <- subset(konza, SOILTYPE %in% c("f", "t"))
 
 
-
-# Data cleanup 
-# 
-# - All `NA` characters should be numeric 0.  
-# 
-# - Extract by transect (watershed), by plot, or by site
-# - Species as columns 
-# - Date as a column
-
-watershed_001d = subset(konza, WATERSHED=="001d")
-watershed_001d_ft <- subset(watershed_001d, SOILTYPE %in% c("f", "t"))
+## Keep only the columns we care about 
 data.cols <- grep("[A-D][1-9]", colnames(konza))
-species.dates <- which(colnames(dat) %in% c("RECYEAR", "RECMONTH", "RECDAY", "SOILTYPE", "AB_GENUS", "AB_SPECIES", "AB_VARIETY"))
-watershed_001d_ft <- watershed_001d_ft[c(data.cols, species.dates)]
-dat <- watershed_001d_ft 
+species.dates <- which(colnames(konza) %in% c("RECYEAR", "RECMONTH", "RECDAY", "SOILTYPE", "AB_GENUS", "AB_SPECIES", "AB_VARIETY"))
+dat <- konza[c(data.cols, species.dates)]
 
 
 # - Combine Species names to single column. (Variety not really necessary)
@@ -76,20 +31,23 @@ dat <- dat[keepme]
 dat$scientific.name <- scientific.name
 
 
-# Combine dates into a single column
-
+# Combine dates into a single column, format as date class
+date <- as.Date(paste(dat$RECYEAR, dat$RECMONTH, dat$RECDAY, sep="-"))
+keepme <- !(colnames(dat) %in% c("RECYEAR", "RECMONTH", "RECDAY"))
+dat <- dat[keepme]
+dat$date <- date
 
 
 # translate abundance codes: code corresponds the midpoint of the percent cover range.  
 abundance_codes <- 
-c("0" = 0,
-  "1" = 0.5,
-  "2" = 3,
-  "3" = 17.5,
-  "4" = 37.5,
-  "5" = 62.5,
-  "6" = 85,
-  "7" = 98.5)
+  c("0" = 0,
+    "1" = 0.5,
+    "2" = 3,
+    "3" = 17.5,
+    "4" = 37.5,
+    "5" = 62.5,
+    "6" = 85,
+    "7" = 98.5)
 
 ## Idetnify the columns that need to be remapped
 data.cols <- grep("[A-D][1-9]", colnames(dat))
@@ -108,7 +66,56 @@ dat[data.cols] <- M # stick the transformed columns back
 
 
 ## Reshape the data into long format: site should be a variable
-A = melt(dat, id = names(dat)[-data.cols])
+A <- melt(dat, id = names(dat)[-data.cols])
 
-B = dcast(A, variable + ... ~ scientific.name, value.var="value", fill = 0, fun.aggregate=max)
+B <- dcast(A, ... ~ scientific.name, value.var="value", fill = 0, fun.aggregate=max)
+dat.obs <- B
 
+
+library(lubridate)
+
+aggregate_by_time <- function(df,
+                              unit = "year",
+                              fun.aggregate = sum,
+                              date.column = which(sapply(df, is, "Date")),
+                              value.columns) # = which(sapply(df, is, "numeric")))
+{
+  
+  df$date_block <- floor_date(df[[date.column]], unit)
+  factors <- names(df)[-c(value.columns, date.column)]
+  
+  out <- ddply(
+    df, 
+    factors,
+    function(x){
+      ap <- apply(x[value.columns], 2, fun.aggregate)
+      do.call(
+        cbind, 
+        c(
+          x[1, -value.columns, drop = FALSE], 
+          as.list(ap), 
+          stringsAsFactors =FALSE
+        )
+        )
+      )
+    })
+}
+
+
+
+
+sp_matrix_over_interval <- function(
+  matrix, 
+  unit = "year",
+  fun.aggregate = "sum",
+  date.column = which(sapply(matrix, is, "Date")),
+  value.columns = which(sapply(matrix, is, "numeric")){
+    long <- melt(matrix, ids = names(matrix)[-value.columns])
+    long[date.column] <- floor_date(long[date.column], unit)
+    dcast(long, 
+          ... ~ names(value.columns),
+          value.var = "value", fill = 0, fun.aggregate=fun.aggregate)
+  }
+  
+  dat <- sp_matrix_over_interval(B, "year", "max")
+  
