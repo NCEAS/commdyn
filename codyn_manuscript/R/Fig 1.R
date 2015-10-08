cbacab521af8b8e3612f0c799884c3fbbfb54222
Dan Flynn <danfbflynn@gmail.com>
2015-10-08 15:58:24 -0700

    fig 1 separate

--- codyn_manuscript/R/Fig 1.R
+++ codyn_manuscript/R/Fig 1.R
@@ -5,12 +5,16 @@
 # use Konza data from Collins et al. 2008 Ecology as the example data set.
 library(vegan)
 library(codyn)
-library(reshape)
+library(ggplot2)
 
 data(collins08)
 
-
 # Initial richness measures. Use transpose_community to create plot x species data frames for vegan
+all.dat <- codyn:::transpose_community(collins08,
+                                   tim = "year",
+                                   spec = "species",
+                                   abun= "abundance")
+
 unb <- codyn:::transpose_community(collins08[collins08$replicate=="unburned",],
                                    tim = "year",
                                    spec = "species",
@@ -24,32 +28,23 @@ ann <- codyn:::transpose_community(collins08[collins08$replicate=="annually burn
                                    )
 
 rich.unb <- apply(unb, 1, function(x) length(x[x>0]))
-
-# rich.unb <- apply(unb, 1, specnumber)
-
 rich.ann <- apply(ann, 1, function(x) length(x[x>0]))
-
-#rich <- t(tapply(collins08$species, list(collins08$replicate, collins08$year), FUN = function(x)
-#          length(unique(x))))
-# melt(rich)
-
 shan.unb <- apply(unb, 1, diversity)
 shan.ann <- apply(ann, 1, diversity)
 
-rad.unb <- radfit(unb)
-rad.ann <- radfit(ann)
+static.dat <- data.frame(year = rep(rownames(ann), 2), 
+           burn = gl(2, nrow(ann), labels = levels(collins08$replicate)),
+           rich = c(rich.ann, rich.unb), 
+           shan = c(shan.ann, shan.unb))
 
-plot(rad.unb)
-plot(rad.ann)
-
-data(BCI)
-mod <- radfit(BCI[3,])
-mod
-plot(mod, pch=".")
-radlattice(mod)
+ggplot(static.dat, aes(year, rich)) + geom_point() + facet_wrap(~burn) + theme_bw()
 
 # Make figures like 08 fig 1. Relative abundance.
+rad.ann <- sort(apply(ann, 2, mean)/max(x), decreasing = T)
+rad.unb <- sort(apply(unb, 2, mean)/max(x), decreasing = T)
 
+
+########## For multiple rank lines in one figure. Matt, ignore this
 rad.ann <- apply(ann, 1, function(x) sort(
         x[x>0]/max(x),
         decreasing = T))
@@ -58,26 +53,42 @@ rad.unb <- apply(unb, 1, function(x) sort(
   x[x>0]/max(x),
   decreasing = T))
 
-plot(1:nrow(ann), seq(0.00001, 1, length.out = nrow(ann)),
+par(mfrow = c(1, 2))
+plot(1:nrow(ann), seq(0.0001, 1, length.out = nrow(ann)),
      ylab = "Relative abundance",
      xlab = "Year",
      type = "n",
-     las = 1,
-     log = "y"
+     las = 2,
+     xlim = c(0, nrow(ann)+1),
+     log = "y",
+     xaxt = "n",
      )
-
-points(x = (1:length(rad.ann$`1984`))/18,
-       y = rad.ann$`1984`, 
-       type = "l"
-       )
-
-# plot(x = (1:length(rad.ann$`1984`))/18,
-#        y = rad.ann$`1984`, log = "y", type = "b")
-
-lapply(rad.ann,
-       function(x)
-         points(x = (1:length(rad.ann$`1984`))/18,
-                y = rad.ann$`1984`, 
+axis(1, at = 1:nrow(ann), labels = rownames(ann))
+count = 1
+for(i in names(rad.ann)){
+    points(x =  (count-1) + ( 1:length(rad.ann[[i]]) ) / 18,
+                y = rad.ann[[i]], 
                 type = "l"
-         )
-       
+                )  
+  count = count + 1
+  }
+
+plot(1:nrow(unb), seq(0.0001, 1, length.out = nrow(unb)),
+     ylab = "Relative abundance",
+     xlab = "Year",
+     type = "n",
+     las = 2,
+     xlim = c(0, nrow(unb)+1),
+     log = "y",
+     xaxt = "n",
+)
+axis(1, at = 1:nrow(unb), labels = rownames(unb))
+count = 1
+for(i in names(rad.unb)){
+  points(x =  (count-1) + ( 1:length(rad.unb[[i]]) ) / 18,
+         y = rad.unb[[i]], 
+         type = "l"
+  )  
+  count = count + 1
+}
+
